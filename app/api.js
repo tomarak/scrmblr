4628fc393b7543c81bee7023408f171cdf010ffa
Anuj Tomar <ankuto@gmail.com>
2015-07-10 16:53:59 -0700

    tested api call

---
+++ app/api.js
@@ -0,0 +1,7 @@
+var getWord = function(){
+  let url = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=7&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
+
+  return fetch(url).then(response=> response.json());
+}
+
+export { getWord }
--- app/main.js
+++ app/main.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import App from './app';
+import {getWord} from './api';
 
 React.render(<App/>, document.body);
+
+getWord().then(word => console.log(word));
+
