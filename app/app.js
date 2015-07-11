1293b03e0bfb4e3b3da648f8f757763938205160
Anuj Tomar <ankuto@gmail.com>
2015-07-10 22:05:45 -0700

    added key listeners

--- app/app.js
+++ app/app.js
@@ -5,31 +5,49 @@ var {Component} = React;
 
 export default class MyApp extends Component {
   //instead of getInitialState, es6 uses the constructor() function
-   constructor(props){
+  constructor(props){
     super(props);
     this.state = { word: '' };
-   }
+  }
 
-   componentDidMount() {
+  componentWillMount() {
+    window.addEventListener("keypress", this.handleKeyPress)
+  }
+  componentWillUnmount() {
+    window.removeEventListener("keypress", this.handleKeyPress);
+  }
+
+  componentDidMount() {
     getWord().then((wordObj) =>
       this.setState({
         wordObj: wordObj,
         scrambled: this.handleScrambleWord(wordObj.word)
       }))
-   }
-   handleScrambleWord (word){
-    var scrambled = word.split('').sort(function(){return 0.5-Math.random()}).join('');
-    return scrambled;
-   }
+  }
 
-   handleGetWord (){
-    return getWord();
-   }
+  handleKeyPress(event) {
+    var keyPressed = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(keyPressed);
+  }
 
-    render () {
-        return (
-            <div>{this.state.scrambled }</div>
+  checkWord(char){
+    var word = this.state.wordObj.word;
+    var index = word.indexOf(char);
+    console.log(index);
+  }
 
-        )
-    }
+  handleScrambleWord (word) {
+    var scrambledWord = word.split('')
+    .sort(function(){return 0.5-Math.random()})
+    .join('');
+    return scrambledWord;
+  }
+
+
+  render () {
+    return (
+      <div >{this.state.scrambled}</div>
+
+      )
+  }
 }
