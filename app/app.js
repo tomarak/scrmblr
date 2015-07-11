aa89d74b62dee6031963856f7578946fa71c8b9b
Anuj Tomar <ankuto@gmail.com>
2015-07-10 17:50:07 -0700

    added scramble word functionality

--- app/app.js
+++ app/app.js
@@ -1,15 +1,35 @@
 import 'whatwg-fetch';
 import React from 'react';
+import {getWord} from './api';
 var {Component} = React;
 
 export default class MyApp extends Component {
-    static propTypes = {
-       flux: React.PropTypes.object.isRequired
+  //instead of getInitialState, es6 uses the constructor() function
+   constructor(props){
+    super(props);
+    this.state = { word: '' };
+   }
+
+   componentDidMount() {
+    getWord().then((wordObj) =>
+      this.setState({
+        wordObj: wordObj,
+        scrambled: this.handleScrambleWord(wordObj.word)
+      }))
+   }
+   handleScrambleWord (word){
+    var scrambled = word.split('').sort(function(){return 0.5-Math.random()}).join('');
+    return scrambled;
+   }
+
+   handleGetWord (){
+    return getWord();
    }
 
     render () {
         return (
-            <div>test</div>
+            <div>{this.state.scrambled }</div>
+
         )
     }
 }
--- app/main.js
+++ app/main.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import App from './app';
-import {getWord} from './api';
 
-React.render(<App/>, document.body);
 
-getWord().then(word => console.log(word));
+React.render(<App/>, document.getElementById('app'));
+
+
 
